f526ca33d3e8b5ba1b69eac8afae9de925a13489
Raniya Delil <raniya@bu.edu>
2025-05-08 18:37:29 -0400

    wrote routes to create task and list all tasks in routes/tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,14 +2,40 @@ const express = require("express");
 const router = express.Router();
 const Task = require("../models/task");
 
-// Get all tasks
-router.get("/tasks", async (req, res) => {
-  res.json([]);
-});
-
 // Create a new task
 router.post("/tasks", async (req, res) => {
-  res.json({});
+  try {
+    const title = req.body.title;
+    if (!title) {
+      return res.json({error: "Please include a title"});
+    }
+    const task = await Task.create({title});
+    res.json(task);
+  } catch (error) {
+    console.error("Error creating task:", error);
+  }
 });
+
+// Read a task
+
+// Mark task as complete
+
+// Update title of task
+
+// Delete a task
+
+// Get all tasks - list all tasks
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.findAll();
+    res.json(tasks);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+  }
+});
+
+// List all completed tasks
+
+// List all pending tasks
 
 module.exports = router;
