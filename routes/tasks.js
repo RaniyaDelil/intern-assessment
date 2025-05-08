ea1cf5f3c0133e4aac4deae363a9e9132a257c8f
Raniya Delil <raniya@bu.edu>
2025-05-08 18:48:58 -0400

    wrote routes for reading task, deleting task, and listing completed or pending tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,6 +6,8 @@ const Task = require("../models/task");
 router.post("/tasks", async (req, res) => {
   try {
     const title = req.body.title;
+
+    // check if title included
     if (!title) {
       return res.json({error: "Please include a title"});
     }
@@ -17,12 +19,42 @@ router.post("/tasks", async (req, res) => {
 });
 
 // Read a task
+router.get("/tasks/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    // check if task exists
+    const task = await Task.findByPk(taskId);
+    if (!task) {
+      return res.json({error: "Task not found"});
+    }
+
+    res.json(task);
+  } catch (error) {
+    console.error("Error fetching task:", error);
+  }
+});
 
 // Mark task as complete
 
 // Update title of task
 
 // Delete a task
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    // check if task exists
+    const task = await Task.findByPk(taskId);
+    if (!task) {
+      return res.json({message: "Task not found"});
+    }
+
+    // delete task
+    await task.destroy();
+    res.json({message: "Task deleted"});
+  } catch (error) {
+    console.error("Error deleting tasks:", error);
+  }
+});
 
 // Get all tasks - list all tasks
 router.get("/tasks", async (req, res) => {
@@ -35,7 +67,23 @@ router.get("/tasks", async (req, res) => {
 });
 
 // List all completed tasks
+router.get("/tasks/completed", async (req, res) => {
+  try {
+    const tasks = await Task.findAll({where: {completed: true}});
+    res.json(tasks);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+  }
+});
 
 // List all pending tasks
+router.get("/tasks/pending", async (req, res) => {
+  try {
+    const tasks = await Task.findAll({where: {completed: false}});
+    res.json(tasks);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+  }
+});
 
 module.exports = router;
